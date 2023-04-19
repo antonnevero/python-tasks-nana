5afcc6b9a756ae10c6c1e5733c4ad963656e33ff
Anton Nevero <nevero.anton@gmail.com>
2023-04-19 14:26:13 +0300

    Add modules

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
+from helper import validate, user_input_message
+
 user_input = ""
-
-
-def days_to_units(num_days, conversion_unit):
-    if conversion_unit == "hours":
-        return f"{num_days} days are {num_days * 24} {conversion_unit}"
-    elif conversion_unit == "minutes":
-        return f"{num_days} days are {num_days * 24 * 60} {conversion_unit}"
-    else:
-        return "unsupported unit"
-
-
-def validate():
-    try:
-        user_input_number = int(days_and_unit_dictionary["days"])
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("You entered a zero")
-        else:
-            print("You entered negative number")
-    except ValueError:
-        print("Your input is not a valid number")
-
-
 while user_input != "exit":
-    user_input = input("Enter a num of days and conversion unit: \n")
+    user_input = input(user_input_message)
     days_and_unit = user_input.split(":")
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    validate()
+    validate(days_and_unit_dictionary)
