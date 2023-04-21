9b114044c693739eb9fc223f197e0d5beed6d38d
Anton Nevero <nevero.anton@gmail.com>
2023-04-21 14:51:07 +0300

    exercise 4 complete

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ for product_row in range(2, product_list.max_row + 1):
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
     product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
 
     # calculation number of products per supplier
     if supplier_name in products_per_supplier:
@@ -31,4 +32,7 @@ for product_row in range(2, product_list.max_row + 1):
     if inventory < 10:
         products_under_10[int(product_num)] = int(inventory)
 
-print(products_under_10)
+    # add value for total inventory price
+    inventory_price.value = inventory * price
+
+inv_file.save("inventory_with_total_value.xlsx")
