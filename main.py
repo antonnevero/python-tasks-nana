d1483d69499703f52fe8d37256919dcdb13a01c8
Anton Nevero <33480281+antonnevero@users.noreply.github.com>
2023-04-24 14:20:41 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from helper import validate, user_input_message
-
-user_input = ""
-while user_input != "exit":
-    user_input = input(user_input_message)
-    days_and_unit = user_input.split(":")
-    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    validate(days_and_unit_dictionary)
