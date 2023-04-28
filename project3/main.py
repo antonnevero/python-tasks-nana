47dd7c5f63f089a1018425817c9f7ac516879c98
Anton Nevero <nevero.anton@gmail.com>
2023-04-28 18:01:39 +0300

    Create project 3

---
+++ project3/main.py
@@ -0,0 +1,8 @@
+from user import User
+
+Anton = User("nn@gmal.com", "Anton", "12345", "DevOps")
+Anton.change_job_title("DevOps engineer")
+Anton.get_user_info()
+
+Veronika = User("vv@gmail.com", "Nika", "11111", "Nail Master")
+Veronika.get_user_info()
