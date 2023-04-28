78006b278e89aeb99e5dc39e8798b641730d7ed8
Anton Nevero <nevero.anton@gmail.com>
2023-04-28 18:08:45 +0300

    Add class Post

--- project3/main.py
+++ project3/main.py
@@ -1,4 +1,5 @@
 from user import User
+from post import Post
 
 Anton = User("nn@gmal.com", "Anton", "12345", "DevOps")
 Anton.change_job_title("DevOps engineer")
@@ -6,3 +7,6 @@ Anton.get_user_info()
 
 Veronika = User("vv@gmail.com", "Nika", "11111", "Nail Master")
 Veronika.get_user_info()
+
+new_post = Post("on a secret mission today", Anton.name)
+new_post.get_post_info()
